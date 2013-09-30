ddb413de180a7101b37cce3e6f754e824426f041
alexfath <alexfath@rams.colostate.edu>
2013-09-30 16:38:32 -0600

    HI

---
+++ CS314PA2/src/HelloWorld.java
@@ -0,0 +1,16 @@
+//CS314PA2
+//Alex Fathman
+//Sep 30, 2013
+//alexfath@rams.colostate.edu
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
